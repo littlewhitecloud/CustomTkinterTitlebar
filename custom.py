ed84b7e42d45a81d9fb98264a4bc057dc9effac5
XiaoBaiYun <71159641+littlewhitecloud@users.noreply.github.com>
2022-12-27 18:01:49 +0800

    1.0.5.4 Update

--- custom.py
+++ custom.py
@@ -1,6 +1,8 @@
 import ctypes
+from winreg import HKEY_CURRENT_USER as hkey
+from winreg import QueryValueEx as getSubkeyValue
+from winreg import OpenKey as getKey
 from tkinter import Tk, Button, Menu, Frame, Label, X, Y, TOP, RIGHT, LEFT
-from winreg import HKEY_CURRENT_USER as hkey, QueryValueEx as getSubkeyValue, OpenKey as getKey
 from ctypes.wintypes import DWORD, BOOL, HRGN, HWND
 from os import getcwd
 try:
@@ -25,37 +27,32 @@ class WINDOWCOMPOSITIONATTRIBDATA(ctypes.Structure):
 		("SizeOfData", ctypes.c_size_t)
 	]
 
-SetWindowCompositionAttribute = ctypes.windll.user32.SetWindowCompositionAttribute
-SetWindowCompositionAttribute.argtypes = (HWND, WINDOWCOMPOSITIONATTRIBDATA)
-SetWindowCompositionAttribute.restype = ctypes.c_int
-
 def hextorgbaint(HEX):
-    alpha = HEX[7:]
-    blue = HEX[5:7]
-    green = HEX[3:5]
-    red = HEX[1:3]
-    gradientColor = alpha + blue + green + red
-    return int(gradientColor, base = 16)
-    
-def blur(hwnd, hexColor = False, Acrylic = False, Dark = False, AccentState = 3):
+	alpha = HEX[7:]
+	blue = HEX[5:7]
+	green = HEX[3:5]
+	red = HEX[1:3]
+	gradientColor = alpha + blue + green + red
+	return int(gradientColor, base = 16)
+
+def blur(hwnd, hexcolor = False, Acrylic = False, Dark = False, AccentState = 3):
 	accent = ACCENTPOLICY()
 	accent.AccentState = 3
-	gradientColor = 0
+	gradientcolor = 0
 	if Acrylic:
 		accent.AccentState = AccentState
-		if hexColor == False:
+		if hexcolor == False:
 			accent.AccentFlags = 2
-			gradientColor = hextorgbaint('#91203801')
-			accent.GradientColor = gradientColor
-
+			gradientcolor = hextorgbaint('#91203801')
+			accent.Gradientcolor = gradientcolor
 	data = WINDOWCOMPOSITIONATTRIBDATA()
 	data.Attribute = 19
 	data.SizeOfData = ctypes.sizeof(accent)
 	data.Data = ctypes.cast(ctypes.pointer(accent), ctypes.POINTER(ctypes.c_int))
-	SetWindowCompositionAttribute(int(hwnd), data)	
-	if Dark: 
+	ctypes.windll.user32.SetWindowCompositionAttribute(int(hwnd), data)
+	if Dark:
 		data.Attribute = 26
-		SetWindowCompositionAttribute(int(hwnd), data)
+		ctypes.windll.user32.SetWindowCompositionAttribute(int(hwnd), data)
         
 def theme():
     valueMeaning = {0: "Dark", 1: "Light"}
@@ -92,7 +89,7 @@ class Tk(Tk):
 		self._t3_hov_load = Image.open(path + '\\togglefull_100.png')
 		self._t3_img = ImageTk.PhotoImage(self._t3_load)
 		self._t3_hov_img = ImageTk.PhotoImage(self._t3_hov_load)
-				
+
 		self.w, self.h = 265, 320 # orginial window size
 		self.o_flag = False
 		self.o_m = False
@@ -129,14 +126,14 @@ class Tk(Tk):
 		self.popup.add_command(label = "最大化", command = self.maxsize)
 		self.popup.add_separator()
 		self.popup.add_command(label = "关闭 (Alt+F4)", command = self.destroy)
-		
+
 		self.titlebar = Frame(self, bg = self.bg, height = 30)
 		self._titleicon = Label(self.titlebar, bg = self.bg)
 		self._titletext = Label(self.titlebar, text = "tk", bg = self.bg, fg = self.colors[self.fg])
 		self._titlemin = Button(self.titlebar, bg = self.bg)
 		self._titlemax = Button(self.titlebar, bg = self.bg)
 		self._titleexit = Button(self.titlebar, bg = self.bg)
-		
+
 		self._titleexit.config(bd = 0,
 			activeforeground = self.colors["exit_fg"],
 			activebackground = self.colors["%sexit_bg" % self.theme],
@@ -182,13 +179,13 @@ class Tk(Tk):
 		self._titlemin.bind("<Leave>", self.min_on_leave)
 		self._titlemax.bind("<Enter>", self.max_on_enter)
 		self._titlemax.bind("<Leave>", self.max_on_leave)
-		
+
 		self._titleicon.bind("<Button-3>", self.popupmenu)
 		self.titlebar.bind("<ButtonPress-1>", self.dragging)
 		self.titlebar.bind("<ButtonRelease-1>", self.stopping)
 		self.titlebar.bind("<B1-Motion>", self.moving)
 		self.titlebar.bind("<Double-Button-1>", self.maxsize)
-		
+
 		self.geometry("%sx%s" % (self.w, self.h))
 		self.iconbitmap(path + "tk.ico")
 		self.wm_iconbitmap(path + "tk.ico")
@@ -201,18 +198,18 @@ class Tk(Tk):
 		WS_CLIPCHILDREN = 0x2000000
 		hwnd = ctypes.windll.user32.GetParent(self.winfo_id())
 		ctypes.windll.user32.SetWindowLongA(hwnd, GWL_STYLE, WS_VISIBLE + WS_CLIPCHILDREN + WS_CLIPSIBLINGS + WS_THICKFRAME)
-	
+
 	def disabledo(self):
 		pass
-	
+
 	def useicon(self, flag = True):
 		if not flag:
 			self._titleicon.pack_forget()
-	
+
 	def usetitle(self, flag = True):
 		if not flag:
 			self._titletext.pack_forget()
-		
+
 	def usemaxmin(self, minsize = True, maxsize = True, minshow = True, maxshow = True):
 		if not minshow:
 			self._titlemin.pack_forget()
@@ -232,11 +229,11 @@ class Tk(Tk):
 	def addblur(self):
 		if self.theme == "dark":
 			hwnd = ctypes.windll.user32.GetForegroundWindow()
-			#blur(hwnd = hwnd, Dark = True, Acrylic = True, AccentState = 3) # Custom AccentState
-	
+			blur(hwnd = hwnd, Dark = True, Acrylic = True, AccentState = 3)
+
 	def popupmenu(self, event):
 		self.popup.post(event.x_root, event.y_root)
-	
+
 	def dragging(self, event):
 		global x, y
 		x = event.x
@@ -254,17 +251,17 @@ class Tk(Tk):
 			self.geometry("+%s+%s" % (self.winfo_x() + deltax, self.winfo_y() + deltay))
 		else:
 			self.resize()
-			
+
 	def focusout(self, event):
 		if self.theme != "light":
 			self.o_f = True
 			self.titlebar["bg"] = self.nf
 			self._titleicon["bg"] = self.nf
-			self._titletext["bg"] = self.nf, 
+			self._titletext["bg"] = self.nf
 			self._titlemin["bg"] = self.nf
 			self._titlemax["bg"] = self.nf
 			self._titleexit["bg"] = self.nf
-	
+
 	def focusin(self, event):
 		if self.theme != "light":
 			self.o_f = False
@@ -282,7 +279,7 @@ class Tk(Tk):
 		self._titlemax["command"] = self.maxsize
 		self._titlemax["image"] = self._t2_hov_img
 		self.o_m = False
-	
+
 	def maxsize(self, event = None):
 		if event and self.o_m == True:
 			self.resize()
@@ -295,34 +292,34 @@ class Tk(Tk):
 			self._titlemax["command"] = self.resize
 			w, h = self.wm_maxsize()
 			self.geometry("%dx%d+0+0" % (w, h - 40))
-	
+
 	def minsize(self):
 		self.overrideredirect(False)
 		self.o_flag = False
 		self.state("iconic")
-		
+
 	def check(self):
 		if self.state() != "iconic" and self.o_flag == False:
 			self.overrideredirect(True)
 			self.o_flag = True
 			self.addblur()
-			
+
 		self.after(100, self.check) # low cpu use
-	
+
 	def exit_on_enter(self, event):
 		if not self.o_f:
 			self._titleexit["background"] = self.colors["exit_fg"]
 			self._titleexit["image"] = self._t0_img
 		else:
 			pass
-			
+
 	def exit_on_leave(self, event):
 		if not self.o_f:
 			self._titleexit["background"] = self.bg
 			self._titleexit["image"] = self._t0_hov_img
 		else:
 			pass
-		
+
 	def min_on_enter(self, event):
 		self._titlemin["image"] = self._t1_img
 
@@ -346,12 +343,13 @@ class Tk(Tk):
 			self._titletext["text"] = text[:15] + "..."
 		else:
 			self._titletext["text"] = text
+
 	def iconbitmap(self, photo):
 		self._icon = Image.open(photo)
-		self._icon = self._icon.resize((16, 16)) 
+		self._icon = self._icon.resize((16, 16))
 		self._img = ImageTk.PhotoImage(self._icon)
 		self._titleicon["image"] = self. _img
-	
+
 	def geometry(self, size):
 		if self.w and self.h:
 			pass
