defcc0133bd11b2925111b1f235b8bc67202ebd3
XiaoBaiYun <71159641+littlewhitecloud@users.noreply.github.com>
2023-01-02 10:00:48 +0800

    Delete upload.sh

--- upload.sh
+++
@@ -1,8 +0,0 @@
-git init
-git add .
-git commit -m "1.0.6.4 Update"
-git branch -M main
-git pull origin master
-git remote add origin git@github.com:littlewhitecloud/CustomTkinterTitlebar.git
-git push -u origin main
-pause
