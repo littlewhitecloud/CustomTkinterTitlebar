6d5dbb501178553a460799fb9a238c6853772279
XiaoBaiYun <71159641+littlewhitecloud@users.noreply.github.com>
2023-01-03 19:29:20 +0800

    1.0.6.8 Update
    merged 1.0.5.5 & 1.0.5.6 Update...
    improved #11
    take seriously I take a lot of time to read setwindowlonga doc...
    (My sw.exe doesn't compile with upx....)
    OHHHHHHHHHHHHHHHHHHHHHHH

--- sw.cpp
+++ sw.cpp
@@ -1,7 +1,6 @@
 /*
 	This is a file about add some long value to change the window.
 	Use function : "SetWindowLong" to change window's attribute.
-	Function in sw.h
 	Include : windows.h
 	HWND gethwnd() {
 		<Get target window's hwnd and return it>
@@ -12,26 +11,23 @@
 		<To change the window's attribute>
 		no args
 	}
-	Function in sw.cpp
-	Include : sw.h
 	int main() {
 		no args
 	}
 */
 #include "windows.h"
-extern "C" {
-	HWND gethwnd() {
-		// Get hwnd by FindWindow
-		HWND hwnd = FindWindow(NULL, L"CTT");
-		return hwnd;
-	}
-	void setwindow() {
-		// Find the hwnd of the window
-		HWND hwnd = gethwnd(); // Get hwnd
-		SetWindowLong(hwnd, GWL_EXSTYLE, WS_EX_TOOLWINDOW | WS_EX_APPWINDOW); // What I improved in 1.0.5.6
-		// SetWindowLong(hwnd, GWL_STYLE, WS_CLIPSIBLINGS | WS_BORDER); // What I improvoed in 1.0.5.5
-	}
-	int main() {
-		setwindow();
-	}
+HWND gethwnd() {
+	// Get hwnd by FindWindow
+	HWND hwnd = FindWindow(NULL, "CTT");
+	return hwnd;
+}
+void setwindow() {
+	// Find the hwnd of the window
+	HWND hwnd = gethwnd(); // Get hwnd
+	SetWindowLong(hwnd, GWL_EXSTYLE, WS_EX_APPWINDOW); // What I improved in 1.0.5.6
+	SetWindowLong(hwnd, GWL_STYLE, WS_VISIBLE | WS_THICKFRAME); // What I improvoed in 1.0.5.5
+}
+int main() {
+	setwindow();
+	return 0;
 }
