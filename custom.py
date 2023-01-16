f8d879ad41097e414dac77b11b087bd93b567763
XiaoBaiYun <71159641+littlewhitecloud@users.noreply.github.com>
2023-01-16 13:35:45 +0800

    1.0.7.3 Update
    This is a little huge update.
    In this update. 
    I replaced the function in class. 
    And add a new function about title, it is still in preview...

--- custom.py
+++ custom.py
@@ -114,7 +114,7 @@ class CTT(Tk):
 		)
 	
 		self._titleicon.pack(fill = Y, side = LEFT, padx = 5, pady = 5)
-		self._titletext.pack(fill = Y, side = LEFT, pady = 1)
+		self._titletext.pack(fill = Y, side = LEFT, pady = 5)
 		self._titleexit.pack(fill = Y, side = RIGHT)
 		self._titlemax.pack(fill = Y, side = RIGHT)
 		self._titlemin.pack(fill = Y, side = RIGHT)
@@ -141,123 +141,67 @@ class CTT(Tk):
 		
 		applywindow(self)
 		self.focus_force()
-		
-	def disabledo(self):
-		""" For disalbe button get even't command """
-		pass
+	
+	# Titlename
+	def title(self, text):
+		""" Rebuild tkinter's title """
+		self._titletext["text"] = text # Will find a good way to show ... if text is too long
+		self.wm_title(text)
+	
+	def title_grey(self):
+		""" ... """
+		self._titletext["foreground"] = "grey"
+	
+	def title_back(self):
+		""" ... """
+		self._titletext["foreground"] = "white"
+	
+	def usetitle(self, flag = True):
+		""" Show / forget titlebar"""
+		if not flag:
+			self._titletext.pack_forget()	
+	
+	def titleconfig(self, pack = "left", font = None):
+		""" Config the titlename """
+		self.usetitle(False)
+		if pack == "left":
+			self._titletext.pack(side = LEFT)
+		elif pack == "right":
+			self._titletext.pack(side = RIGHT)
+		else:
+			self._titletext.config(justify = "center")
+			self._titletext.pack(expand = True)
+		if font:
+			self._titletext.config(font = font)
+	
+	# Titleicon
 	
 	def useicon(self, flag = True):
 		""" Show icon """
 		if not flag:
 			self._titleicon.pack_forget()
 	
-	def usetitle(self, flag = True):
-		""" Show / forget titlebar"""
-		if not flag:
-			self._titletext.pack_forget()
-			
-	def usemaxmin(self, minsize = True, maxsize = True, minshow = True, maxshow = True):
-		""" Show / Disable min / max button """
-		if not minshow:
-			self._titlemin.pack_forget()
-		elif not minsize:
-			self.min_on_enter(None)
-			self._titlemin["command"] = self.disabledo
-			self._titlemin.unbind("<Leave>")
-			self._titlemin.unbind("<Enter>")
-		if not maxshow:
-			self._titlemax.pack_forget()
-		elif not maxsize:
-			self.max_on_enter(None)
-			self._titlemax["command"] = self.disabledo
-			self._titlemax.unbind("<Leave>")
-			self._titlemax.unbind("<Enter>")
-	
-	def addblur(self, acrylic = True, dark = isDark()):
-		""" Add blur / acrylic effect to window """
-		if dark:
-			self.focus_force()
-			hwnd = windll.user32.GetForegroundWindow()
-			blur(hwnd = hwnd, hexColor = '#91203801',Dark = dark, Acrylic = acrylic)
-	
 	def popupmenu(self, event):
 		""" Popup menu """
 		self.popup.post(event.x_root, event.y_root)
-		
-	def dragging(self, event):
-		""" Start drag window """
-		global x, y
-		x = event.x
-		y = event.y
 
-	def moving(self, event):
-		""" Window moving """
-		global x, y
-		if not self.o_m:
-			new_x, new_y = (event.x - x), (event.y - y)
-			mw.moving(self.winfo_x(), self.winfo_y(), new_x, new_y)
-		else:
-			self.resize()
+	def iconphoto(self, image):
+		""" Rebuild tkinter's iconphoto """
+		self._icon = Image.open(image)
+		self._icon = self._icon.resize((16, 16))
+		self._img = ImageTk.PhotoImage(self._icon)
+		self._titleicon["image"] = self. _img
+		self.wm_iconphoto(self._img)
 
-	def focusout(self, event):
-		""" When focusout """
-		self.exit_grey()
-		self.min_grey()
-		self.max_grey()
-		self.o_f = True
-		self.titlebar["bg"] = self.nf
-		self._titleicon["bg"] = self.nf
-		self._titletext["bg"] = self.nf
-		self._titlemin["bg"] = self.nf
-		self._titlemax["bg"] = self.nf
-		self._titleexit["bg"] = self.nf
+	def iconbitmap(self, image):
+		""" Rebuild tkinter's iconbitmap """
+		self._icon = Image.open(image)
+		self._icon = self._icon.resize((16, 16))
+		self._img = ImageTk.PhotoImage(self._icon)
+		self._titleicon["image"] = self. _img
+		self.wm_iconbitmap(image)
 
-	def focusin(self, event):
-		""" When focusin """
-		self.o_f = False
-		self.exit_back()
-		self.min_back()
-		self.max_back()
-		self.titlebar["bg"] = self.bg
-		self._titleicon["bg"] = self.bg
-		self._titletext["bg"] = self.bg
-		self._titlemin["bg"] = self.bg
-		self._titlemax["bg"] = self.bg
-		self._titleexit["bg"] = self.bg
-
-	def resize(self):
-		""" Resize window """ 
-		self.popup.entryconfig("Restore", state = "disabled")
-		self.popup.entryconfig("Maxsize", state = "active")
-		self.wm_geometry("%dx%d+%d+%d" % (int(self.w), int(self.h), int(self.w_x), int(self.w_y)))
-		self._titlemax["command"] = self.maxsize
-		self._titlemax["image"] = self._t2_hov_img
-		self.o_m = False
-
-	def maxsize(self, event = None):
-		""" Maxsize Window """
-		if event and self.o_m:
-			self.resize()
-		else:
-			self.popup.entryconfig("Restore", state = "active")
-			self.popup.entryconfig("Maxsize", state = "disabled")
-			self.w_x, self.w_y = self.winfo_x(), self.winfo_y()
-			self.o_m = True
-			self._titlemax["image"] = self._t3_hov_img
-			self._titlemax["command"] = self.resize
-			w, h = self.wm_maxsize()
-			self.sg("%dx%d+0+0" % (w, h - 40))
-	
-	def deminsize(self, event):
-		""" Deminsize window """
-		self.attributes("-alpha", 1)
-		self.bind("<FocusIn>", self.focusin)
-		
-	def minsize(self):
-		""" Minsize window """
-		self.attributes("-alpha", 0)
-		self.bind("<FocusIn>", self.deminsize)
-	
+	# Titlebutton
 	def exit_on_enter(self, event = None):
 		""" ... """
 		if not self.o_f:
@@ -275,7 +219,7 @@ class CTT(Tk):
 	def exit_back(self, event = None):
 		""" ... """
 		self._titleexit["image"] = self._t0_hov_img
-			
+		
 	def min_grey(self, event = None):
 		""" ... """
 		self._titlemin["image"] = self._t1_img
@@ -298,25 +242,109 @@ class CTT(Tk):
 		else:
 			self._titlemax["image"] = self._t3_hov_img
 
-	def title(self, text):
-		""" Rebuild tkinter's title """
-		if len(text) > 15:
-			text = text[:15] + "..."
-		self._titletext["text"] = text
-		self.wm_title(text)
+	def disabledo(self):
+		""" For disalbe button get even't command """
+		pass
 
-	def iconbitmap(self, image):
-		""" Rebuild tkinter's iconbitmap """
-		self._icon = Image.open(image)
-		self._icon = self._icon.resize((16, 16))
-		self._img = ImageTk.PhotoImage(self._icon)
-		self._titleicon["image"] = self. _img
-		self.wm_iconbitmap(image)
+	def usemaxmin(self, minsize = True, maxsize = True, minshow = True, maxshow = True):
+		""" Show / Disable min / max button """
+		if not minshow:
+			self._titlemin.pack_forget()
+		elif not minsize:
+			self.min_on_enter(None)
+			self._titlemin["command"] = self.disabledo
+			self._titlemin.unbind("<Leave>")
+			self._titlemin.unbind("<Enter>")
+		if not maxshow:
+			self._titlemax.pack_forget()
+		elif not maxsize:
+			self.max_on_enter(None)
+			self._titlemax["command"] = self.disabledo
+			self._titlemax.unbind("<Leave>")
+			self._titlemax.unbind("<Enter>")
+
+	# Window
+
+	def moving(self, event):
+		""" Window moving """
+		global x, y
+		if not self.o_m:
+			new_x, new_y = (event.x - x), (event.y - y)
+			mw.moving(self.winfo_x(), self.winfo_y(), new_x, new_y)
+		else:
+			self.resize()
+
+	def dragging(self, event):
+		""" Start drag window """
+		global x, y
+		x = event.x
+		y = event.y
+
+	def maxsize(self, event = None):
+		""" Maxsize Window """
+		if event and self.o_m:
+			self.resize()
+		else:
+			self.popup.entryconfig("Restore", state = "active")
+			self.popup.entryconfig("Maxsize", state = "disabled")
+			self.w_x, self.w_y = self.winfo_x(), self.winfo_y()
+			self.o_m = True
+			self._titlemax["image"] = self._t3_hov_img
+			self._titlemax["command"] = self.resize
+			w, h = self.wm_maxsize()
+			self.sg("%dx%d+0+0" % (w, h - 40))
+
+	def resize(self):
+		""" Resize window """ 
+		self.popup.entryconfig("Restore", state = "disabled")
+		self.popup.entryconfig("Maxsize", state = "active")
+		self.wm_geometry("%dx%d+%d+%d" % (int(self.w), int(self.h), int(self.w_x), int(self.w_y)))
+		self._titlemax["command"] = self.maxsize
+		self._titlemax["image"] = self._t2_hov_img
+		self.o_m = False
+
+	def minsize(self):
+		""" Minsize window """
+		self.attributes("-alpha", 0)
+		self.bind("<FocusIn>", self.deminsize)
+
+	def deminsize(self, event):
+		""" Deminsize window """
+		self.attributes("-alpha", 1)
+		self.bind("<FocusIn>", self.focusin)
+
+	def focusout(self, event):
+		""" When focusout """
+		self.exit_grey()
+		self.min_grey()
+		self.max_grey()
+		self.title_grey()
+		self.o_f = True
+		self.titlebar["bg"] = self.nf
+		self._titleicon["bg"] = self.nf
+		self._titletext["bg"] = self.nf
+		self._titlemin["bg"] = self.nf
+		self._titlemax["bg"] = self.nf
+		self._titleexit["bg"] = self.nf
+
+	def focusin(self, event):
+		""" When focusin """
+		self.o_f = False
+		self.exit_back()
+		self.min_back()
+		self.max_back()
+		self.title_back()
+		self.titlebar["bg"] = self.bg
+		self._titleicon["bg"] = self.bg
+		self._titletext["bg"] = self.bg
+		self._titlemin["bg"] = self.bg
+		self._titlemax["bg"] = self.bg
+		self._titleexit["bg"] = self.bg
 
 	def close(self, event = None):
 		""" Close Window """
 		self.destroy()
-	
+
 	def geometry(self, w, h):
 		""" Change the self.w and self.h forcely """
 		self.w, self.h = w, h
@@ -329,7 +357,7 @@ class CTT(Tk):
 		else:
 			self.w, self.h = size.split('x')[0], size.split('x')[1]
 		self.wm_geometry(size)		
-	
+
 	def settheme(self, theme):
 		if theme == "dark":
 			self.theme = "dark"
@@ -342,7 +370,15 @@ class CTT(Tk):
 			self.bg = self.colors["light"]
 			self.nf = self.colors["light_nf"]
 			self.fg = "dark"
+
+	def addblur(self, acrylic = True, dark = isDark()):
+		""" Add blur / acrylic effect to window """
+		if dark:
+			self.focus_force()
+			hwnd = windll.user32.GetForegroundWindow()
+			blur(hwnd = hwnd, hexColor = '#91203801',Dark = dark, Acrylic = acrylic)
+
 if __name__ == "__main__":
 	example = CTT() # Test
-	#example.addblur()
+	example.addblur()
 	example.mainloop()
