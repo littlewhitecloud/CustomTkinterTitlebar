6e8b0c108984094d4b278a35a4c390a8929cabfa
XiaoBaiYun <71159641+littlewhitecloud@users.noreply.github.com>
2023-01-25 19:20:32 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ echo Require VS 2022
 echo If you set the cl.exe in system path, you can use this file to build dll in normal console. 
 echo Use x64 Native Tools Command Prompt for VS 2022 to build 64 bit dll
 echo Use x86 Native Tools Command Prompt for VS 2022 to build 32 bit dll
-echo You can open the command prompt can use "cd" to this path and do ".\build.bat"
+echo You can open the Developer PowerShell for VS 2022 and use "cd" to this path do ".\build.bat"
 echo And the "cl.exe" will build the dll ...
 echo Start building the pluguin.dll ...
 cl /LD /DEF: plugin.def plugin.cpp /std:c++20
