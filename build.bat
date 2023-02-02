42a879b43e7343fd63d3a18e700d3f392efcc1e9
XiaoBaiYun <71159641+littlewhitecloud@users.noreply.github.com>
2023-02-02 17:53:11 +0800

    use C instead C++

--- build.bat
+++ build.bat
@@ -12,13 +12,13 @@ echo You can exit and use the command prompt or press any key to continue...
 pause
 
 echo Start install the inneed package
-python -m pip install --upgrade pip
-pip install pillow
-pip install darkdetect
-pip install blurwindow
+python -m pip install --upgrade pip --user
+pip install pillow --user
+pip install darkdetect --user
+pip install blurwindow --user
 
 echo Start building the plugin.dll ...
 echo You can open the Developer PowerShell for VS 2022 and use "cd" to this path do ".\build.bat"
 echo And the "cl.exe" will build the dll automatic...
-cl /O2 /Ot /nologo /errorReport:none /w /favor:INTEL64 /fp:fast /GA /EHsc /analyze- /D "_WINDOWS" /std:c++latest /LD /DEF: plugin.def plugin.cpp
+cl /O2 /Ot /nologo /errorReport:none /w /favor:INTEL64 /fp:fast /GA /EHsc /analyze- /D "_WINDOWS" /std:c++latest /LD /DEF: plugin.def plugin.c
 pause
---
+++ plugin.c
@@ -0,0 +1,17 @@
+/*
+	Include : windows.h; Compile with : plugin.def;
+	__declspec(dllexport) void move(HWND hwnd, int x, int y, int eventx, int eventy) {
+		"x : windowx | y : windowy | eventx : mousex | eventy : mousey"
+		<move window's funcition>
+	}
+	__declspec(dllexport) void setwindowlong(HWND hwnd) {
+		<To change the window's attribute>
+	}
+*/
+#include <windows.h>
+#pragma comment(lib, "user32.lib")
+__declspec(dllexport) inline void move(HWND hwnd, int x, int y, int eventx, int eventy) { SetWindowPos(hwnd, NULL, eventx + x, eventy + y, 0, 0, SWP_NOREDRAW | SWP_NOSIZE | SWP_SHOWWINDOW); } // x : windowx | y : windowy | eventx : mousex | eventy : mousey
+__declspec(dllexport) void setwindow(HWND hwnd) {
+	SetWindowLong(hwnd, GWL_EXSTYLE, WS_EX_APPWINDOW);
+	SetWindowLong(hwnd, GWL_STYLE, WS_VISIBLE | WS_THICKFRAME);
+}
