e514d69874d196e43e8b1565a3cfa37e77726e0c
XiaoBaiYun <71159641+littlewhitecloud@users.noreply.github.com>
2023-05-18 09:29:30 +0800

    l love you metamorphosis batch file

--- build.bat
+++ build.bat
@@ -1,23 +1,8 @@
 echo off
 cls
-echo You can use this file to build the project
-echo Require Visual Studio and Python
 
-echo Tip
-echo If you set the cl.exe in system path, you can use this file to build dll in normal console
-echo If not, you can do this:
-echo Use x64 Native Tools Command Prompt for VS 2022 to build 64 bit dll
-echo Use x86 Native Tools Command Prompt for VS 2022 to build 32 bit dll
-echo You can exit and use the command prompt or press any key to continue...
-pause
-
-echo Start install the inneed package
 python -m pip install --upgrade pip --user
 pip install pillow --user
 pip install darkdetect --user
 pip install blurwindow --user
-
-echo Start building the plugin.dll ...
-echo You can open the Developer PowerShell for VS 2022 and use "cd" to this path do ".\build.bat"
-echo And the "cl.exe" will build the dll automatic...
 cl /Ox /Ot /favor:blend /EHsc /GA /nologo /LD /DEF: plugin.def plugin.c /w /errorReport:none /std:c17 /D "_WINDOWS"
