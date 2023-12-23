615511594c046b31c4d943593e711dc3b66df0c1
小白云 <71159641+littlewhitecloud@users.noreply.github.com>
2023-12-23 18:32:58 +0800

    Delete build.bat

--- build.bat
+++
@@ -1,4 +0,0 @@
-pip install -r requirements.txt
-
-rem build the dll file if you want, require Visual Studio 2022
-rem cl /Ox /Ot /favor:blend /EHsc /GA /nologo /LD /DEF: plugin.def plugin.c /w /errorReport:none /std:c17 /D "_WINDOWS"
