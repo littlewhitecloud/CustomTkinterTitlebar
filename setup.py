b8e847ab332ca8c61041da330edbdb3f873f317b
小白云 <71159641+littlewhitecloud@users.noreply.github.com>
2024-01-09 20:39:32 +0800

    wat

--- setup.py
+++ setup.py
@@ -3,15 +3,11 @@ from distutils.core import setup
 with open("README.md", "r", encoding="utf-8") as readme:
     long_description = readme.read()
 
-requires = [
-    "Pillow>=9.0.0",
-    "BlurWindow>=1.2.1",
-    "darkdetect>=0.8.0"
-]
+requires = ["Pillow>=9.0.0", "darkdetect>=0.8.0"]
 
 setup(
     name="CustomTkinterTitlebar",
-    version="1.0.7.9",
+    version="1.0.8.0",
     authior="littlewhitecloud",
     author_email="q1141926647@163.com",
     description="This is a 📚project can help you to have a custom titlebar! 这是一个可以创建自定义标题栏的📚项目",
@@ -19,24 +15,18 @@ setup(
     long_description_content_type="text/markdown",
     author="littlewhitecloud",
     url="https://github.com/littlewhitecloud/CustomTkinterTitlebar",
-    
-    package="CustomTkinterTitlebar",
-    install_requires = requires,
+    package="customtitlebar",
+    install_requires=requires,
     licrnse="MIT",
-    include_package_data = True,
-    classifiers = [
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-
-        'License :: OSI Approved :: MIT License',
-
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
-    package_data={
-         '':["*.dll"]
-    },
-
 )
